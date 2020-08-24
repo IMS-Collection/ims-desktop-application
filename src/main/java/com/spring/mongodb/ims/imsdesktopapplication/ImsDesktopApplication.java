a766057ebe51e43c8f533619856bb3a5c8fc290e
Hyacinth Ali <hyacinth.ali@mail.mcgill.ca>
2020-08-23 23:30:47 -0400

    first commit

---
+++ src/main/java/com/spring/mongodb/ims/imsdesktopapplication/ImsDesktopApplication.java
@@ -0,0 +1,13 @@
+package com.spring.mongodb.ims.imsdesktopapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImsDesktopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImsDesktopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/mongodb/ims/imsdesktopapplication/ImsDesktopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.mongodb.ims.imsdesktopapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImsDesktopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
