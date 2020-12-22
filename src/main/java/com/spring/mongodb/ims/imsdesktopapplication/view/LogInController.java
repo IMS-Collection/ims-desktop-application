f616f91818a5941002496bb8e1989835633a0741
Hyacinth Ali <hyacinth.ali@mail.mcgill.ca>
2020-12-22 01:45:32 -0500

    login button action connected

--- src/main/java/com/spring/mongodb/ims/imsdesktopapplication/view/LogInController.java
+++ src/main/java/com/spring/mongodb/ims/imsdesktopapplication/view/LogInController.java
@@ -1,5 +1,12 @@
 package com.spring.mongodb.ims.imsdesktopapplication.view;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class LogInController extends BaseController {
+	
+	public void login() {
+		System.out.println("Login button clicked");
+	}
 
 }
